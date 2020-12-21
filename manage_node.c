c4f0a3463b92458f5eb943e74e43eae7e4f6bc2a
yagomfh <yagomfh@gmail.com>
2020-12-21 15:59:53 +0000

    feat: add node end

--- manage_node.c
+++ manage_node.c
@@ -94,3 +94,35 @@ size_t stack_len(stack_t *h)
 	len++;
 	return (len);
 }
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: pointer to head node
+ * @n: data of node
+ * Return: address of the new element, or NULL if it failed
+ */
+
+stack_t *add_dnodeint_end(stack_t **head, const int n)
+{
+	stack_t *new, *last = *head;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		while (last->next)
+			last = last->next;
+		new->prev = last;
+		last->next = new;
+	}
+	return (new);
+}

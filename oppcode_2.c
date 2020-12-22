4992c7b211023cb1f0f8b689f14f3cbed846dded
Thibaut <thibautbernard@sfr.fr>
2020-12-22 12:22:08 +0000

    fix():

--- oppcode_2.c
+++ oppcode_2.c
@@ -15,7 +15,7 @@ void _pstr(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 	while (head != NULL)
 	{
-		if (head->n == 0 || head->n <= 0 || head->n > 127)
+		if (head->n <= 0 || head->n > 127)
 			break;
 		printf("%c", head->n);
 		head = head->next;
